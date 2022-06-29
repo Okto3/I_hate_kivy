33f7e488fa67289071445263fc529dd50d2340ca
Okto3 <73514013+Okto3@users.noreply.github.com>
2022-06-29 22:23:56 +1000

    fixed lyrics page and trans progessing

--- kivyApp.py
+++ kivyApp.py
@@ -4,33 +4,43 @@ from kivy.base import runTouchApp
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, SlideTransition, TransitionBase
 from kivymd.app import MDApp
 import json
 import urlrequest
 from kivy.animation import Animation
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.scrollview import ScrollView
 import time
 import random
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy_garden.mapview import MapView
+
 
 lyrics = ""
+songTitle = ""
+songArtist = ""
 
 class FirstScreen(Screen):
     pass
 class SearchLyricsScreen(Screen):
     pass
 class StartScreen(Screen):
+    def mapView(self):
+        mapview = MapView(zoom=12, lat=55.6712674, lon=12.5938239)
+class EventRego(Screen):
     pass
 class displayLyricsScreen(Screen,BoxLayout):
     getLyrics = StringProperty()
     message = StringProperty()
     btn = ObjectProperty(None)
-    def change_text(self):
-        self.getLyrics = lyrics
-    def removeButton(self):
-        self.remove_widget(self.btn)
+
+    def on_enter(self):
+        Clock.schedule_once(self.dispLyrics)
+
+    def dispLyrics(self,dt):    
+        self.ids.lyricsLable.text = lyrics
 
 
 
@@ -82,259 +92,12 @@ class MyScreenManager(ScreenManager):
         #print(lyrics)
         self.current = 'displayLyricsPage'
         return lyrics
-
-    
-    def getLyricsScreen(self):
-        self.current = 'SearchLyricsPage'  
-    def backToHomeScreen(self):
-        self.current = 'start'  
          
     
 
     
 
-
-
-
-root_widget = Builder.load_string('''
-#:import FadeTransition kivy.uix.screenmanager.FadeTransition
-MyScreenManager:
-    transition: FadeTransition()
-    FirstScreen:
-    StartScreen:
-    SearchLyricsScreen:
-    displayLyricsScreen:
-    
-<FirstScreen>:
-    name: 'loginPage'
-    size_hint: None, None
-    size: 300,533
-    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-    elevation: 10
-    padding: 25
-    spacing: 25
-    orientation: 'vertical'
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 1
-        Rectangle:
-            size: self.size
-            
-    BoxLayout:
-        orientation: 'vertical'
-        padding: 25
-        spacing: 25
-        Label:
-            id: welcome_label
-            text: "WELCOME"
-            font_size:40
-            halign: 'center'
-            valign: 'top'
-            color: 0.3,0.3,0.3,1
-            size_hint_y: 1
-            height: self.texture_size[1]
-            padding_y:30
-        
-        TextInput:
-            id: username
-            hint_text: "username"
-            font_size: 18
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 35
-            multiline: False
-            
-        
-        TextInput:
-            id: password
-            hint_text: "password"
-            font_size: 18
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 35
-            multiline: False
-            spacing: 10
-
-        Button:
-            id: submitLoginButton
-            text: "Submit"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: 
-                app.root.getLogin()
-        
-        Button:
-            id: registerButton
-            text: "Register"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: 
-                app.root.getRegistration()
-                
-<StartScreen>:
-    name: 'start'
-    size_hint: None, None
-    size: 300,533
-    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-    elevation: 10
-    padding: 25
-    spacing: 25
-    orientation: 'vertical'
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 1
-        Rectangle:
-            size: self.size
-    
-    Label:
-        id: songTitleLable
-        text: "Upcoming Karaoke Events"
-        font_size:40
-        halign: 'center'
-        pos_hint: {"x":0,"y":0.8}
-        color: 0.3,0.3,0.3,1
-        padding_y:30
-        text_size: self.width, None
-        size_hint: 1, None
-            
-    BoxLayout:
-        orientation: 'vertical'
-        padding: 25
-        spacing: 25
-        
-
-        Button:
-            id: submitSongTitle
-            text: "Get Lyrics"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: 
-                app.root.getLyricsScreen()
-
-
-<SearchLyricsScreen>:
-    name: 'SearchLyricsPage'
-    size_hint: None, None
-    size: 300,533
-    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-    elevation: 10
-    padding: 25
-    spacing: 25
-    orientation: 'vertical'
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 1
-        Rectangle:
-            size: self.size
-            
-    BoxLayout:
-        orientation: 'vertical'
-        padding: 25
-        spacing: 25
-        Label:
-            id: songTitleLable
-            text: "Seach For Song"
-            font_size:40
-            halign: 'center'
-            valign: 'top'
-            color: 0.3,0.3,0.3,1
-            height: self.texture_size[1]
-            padding_y:30
-            text_size: self.width, None
-            size_hint: 1, None
-        
-        TextInput:
-            id: songTitle
-            hint_text: "Title"
-            font_size: 18
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 35
-            multiline: False
-        
-        TextInput:
-            id: artist
-            hint_text: "Artist"
-            font_size: 18
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 35
-            multiline: False
-
-        Button:
-            id: submitSongTitle
-            text: "Get Lyrics"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: app.root.getLyrics()
-        Button:
-            id: backToHome
-            text: "Back"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: app.root.backToHomeScreen()
-
-<displayLyricsScreen>:
-    btn: remove
-    name: 'displayLyricsPage'
-    size_hint: None, None
-    size: 300,533
-    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-    elevation: 10
-    padding: 25
-    spacing: 25
-    orientation: 'vertical'
-    canvas.before:
-        Color:
-            rgba: 1, 1, 1, 1
-        Rectangle:
-            size: self.size
-    BoxLayout:
-        orientation: 'vertical'
-        height: self.minimum_height
-        ScrollView:
-            do_scroll_x: False
-            do_scroll_y: True
-            Label:
-                text: "start"
-                text: root.getLyrics
-                font_size:20
-                halign: 'center'
-                pos_hint: {"x":0,"y":0.8}
-                color: 0.3,0.3,0.3,1
-                padding_y:30
-                text_size: self.width, None
-                size_hint: 1, None
-        Button:
-            text: "start"
-            id: remove
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            on_press: 
-                root.removeButton()
-                root.change_text()
-        Button:
-            id: backToHome
-            text: "Back"
-            pos_hint: {"centre_x":0.5}
-            size_hint: (1, None)
-            height: 40
-            multiline: False
-            on_release: app.root.backToHomeScreen()
-                
-
-''')
+root_widget = Builder.load_file("styleApp.kv")
 
 
 
